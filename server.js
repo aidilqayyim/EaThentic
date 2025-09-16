65506d1e9b23c459d10d76717da2eb8e1b966ef4
Aidil Qayyim <muhdaidilqayyim@gmail.com>
2025-09-17 00:04:57 +0800

    set limit on number of reviews to fetch to 3 pages

--- server.js
+++ server.js
@@ -77,7 +77,7 @@ app.get("/reviews", async (req, res) => {
     // Stage 1
     sendStage("Getting reviews", "done", "green");
 
-    for (let i = 0; i < 6; i++) {
+    for (let i = 0; i < 3; i++) {
       const body = token
         ? { placeId, nextPageToken: token, gl: "my" }
         : { placeId, gl: "my" };
